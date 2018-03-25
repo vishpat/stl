f07a2c6ab447ae2892409077a68ce8fb87374172
Vishal Patil <vishpat@gmail.com>
2018-03-24 23:46:20 -0400

    Added unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,23 @@
-
 #[cfg(test)]
 mod tests {
+    use stl;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn binary_format_check() {
+        let fmt = stl::parser::get_format("/home/vpati011/Downloads/ship_v2_top.stl".to_string());
+        match fmt {
+            stl::parser::FileFormat::Binary => println!("Pass"),
+            _ => panic!("Test failed for detect the binary format for the STL file"),
+        }
+    }
+
+    #[test]
+    fn text_format_check() {
+        let fmt = stl::parser::get_format("/home/vpati011/Downloads/cube.stl".to_string());
+        match fmt {
+            stl::parser::FileFormat::Text => println!("Pass"),
+            _ => panic!("Test failed for detect the text format for the STL file"),
+        }
     }
 }
 
@@ -14,6 +28,7 @@ pub mod stl {
         use std::fs::File;
         use std::io::Read;
 
+        #[derive(Debug)]
         pub enum FileFormat {
             Invalid,
             Text,
@@ -24,10 +39,10 @@ pub mod stl {
             let mut stl_file = File::open(stl_file_path).expect("Unable to open the stl file");
             let mut buf = [0; 80];
             
-            stl_file.read_exact(&mut buf).expect("Unable to read first 5 bytes");
+            stl_file.read_exact(&mut buf).expect("Unable to the STL file header");
             let header = String::from_utf8(buf.to_vec()).expect("Unable convert read buffer to string");
            
-            if header.trim().starts_with("solid") {
+            if header.trim().to_lowercase().starts_with("solid") {
                 FileFormat::Text
             } else {
                 FileFormat::Binary

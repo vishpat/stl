8b85ac966a8577582802f64678801e8e05eb1235
Vishal Patil <vishpat@gmail.com>
2018-03-25 10:09:16 -0400

    Simplified code

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ mod tests {
     fn binary_format_check() {
         let fmt = stl::parser::get_format("/home/vpati011/Downloads/ship_v2_top.stl".to_string());
         match fmt {
-            stl::parser::FileFormat::Binary => println!("Pass"),
+            Ok(format) => println!("Pass"),
             _ => panic!("Test failed for detect the binary format for the STL file"),
         }
     }
@@ -15,7 +15,7 @@ mod tests {
     fn text_format_check() {
         let fmt = stl::parser::get_format("/home/vpati011/Downloads/cube.stl".to_string());
         match fmt {
-            stl::parser::FileFormat::Text => println!("Pass"),
+            Ok(format) => println!("Pass"),
             _ => panic!("Test failed for detect the text format for the STL file"),
         }
     }
@@ -27,25 +27,51 @@ pub mod stl {
 
         use std::fs::File;
         use std::io::Read;
+        use std;
+
+        const HEADER_SIZE: usize = 80;
 
         #[derive(Debug)]
         pub enum FileFormat {
-            Invalid,
             Text,
             Binary,
         }
 
-        pub fn get_format(stl_file_path: String) -> FileFormat {
-            let mut stl_file = File::open(stl_file_path).expect("Unable to open the stl file");
-            let mut buf = [0; 80];
+        pub enum FileFormatError {
+            InvalidPath(std::io::Error),
+            InvalidRead(std::io::Error),
+            InvalidFormat(std::string::FromUtf8Error),
+        }
+
+        pub enum ModelError {
+            STLFileInvalid,
+        }
+
+        pub struct Vertex {
+            x: f32,
+            y: f32,
+            z: f32,
+        }
+
+        pub struct Triangle { 
+            sides:[Vertex; 3],
+        }
+
+        pub struct Model {
+            triangles: Vec<Triangle>,
+        }
+
+        pub fn get_format(stl_file_path: String) -> Result<FileFormat, FileFormatError> {
+            let mut stl_file = File::open(stl_file_path).map_err(FileFormatError::InvalidPath)?;
+            let mut buf = [0; HEADER_SIZE];
             
-            stl_file.read_exact(&mut buf).expect("Unable to the STL file header");
-            let header = String::from_utf8(buf.to_vec()).expect("Unable convert read buffer to string");
+            stl_file.read_exact(&mut buf).map_err(FileFormatError::InvalidRead)?;
+            let header = String::from_utf8(buf.to_vec()).map_err(FileFormatError::InvalidFormat)?;
            
             if header.trim().to_lowercase().starts_with("solid") {
-                FileFormat::Text
+                Ok(FileFormat::Text)
             } else {
-                FileFormat::Binary
+                Ok(FileFormat::Binary)
             }
         }
     }
